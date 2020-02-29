a39365b1dbc34d7457e742cf74b2e03cc557a01a
Thais Carvalho <tata1br06@Thaiss-MacBook-Air.local>
2020-02-28 19:21:08 -0500

    Codewars

---
+++ problemFive.js
@@ -0,0 +1,8 @@
+// Problem #5
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+//
+// Complete the solution so that it reverses the string value passed into it.
+
+function solution(str){
+return str.split("").reverse().join("")
+}
