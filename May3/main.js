d1bd87a5f56739183e8ac51ff432d09b17dee92c
Thais Nascimento <60048253+Thais1Nascimento1@users.noreply.github.com>
2020-03-03 21:20:05 -0500

    Add files via upload

---
+++ May3/main.js
@@ -0,0 +1,19 @@
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+//
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//   // code goes here
+// }
+
+// solution:
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  const messiGoals=laLigaGoals+ copaDelReyGoals+ championsLeagueGoals
+  return messiGoals
+}
